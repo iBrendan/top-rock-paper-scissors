34389d169baf991071f98896b09b5308bb57b9fd
iBrendan <imohbren@gmail.com>
2022-08-07 22:51:40 -0400

    Create function for computer to randomly return choice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("test");
+//Computer randomly returns one of "Rock, "Paper" or "Scissors" from array
+function getComputerChoice() {
+  const choice = ["rock", "paper", "scissors"];
+  return choice[Math.floor(Math.random() * choice.length)];
+}
+
+// console.log(getComputerChoice());
