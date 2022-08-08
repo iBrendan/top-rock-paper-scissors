cac3b72f530b18a837bcc1fed8672bcb7df8f770
iBrendan <imohbren@gmail.com>
2022-08-07 23:03:21 -0400

    Add function to play single game round

--- script.js
+++ script.js
@@ -4,4 +4,37 @@ function getComputerChoice() {
   return choice[Math.floor(Math.random() * choice.length)];
 }
 
-// console.log(getComputerChoice());
+let computerScore = 0;
+let playerScore = 0;
+// Single round of Rock-Paper-Scissors
+function playRound(playerSelection, computerSelection) {
+  if (
+    playerSelection.toLowerCase() === "rock" &&
+    computerSelection.toLowerCase() === "paper"
+  ) {
+    computerScore++;
+    return `Computer played ${computerSelection} and you played ${playerSelection} so you lose!`;
+  } else if (
+    playerSelection.toLowerCase() === "scissors" &&
+    computerSelection.toLowerCase() === "rock"
+  ) {
+    computerScore++;
+    return `Computer played ${computerSelection} and you played ${playerSelection} so you lose!`;
+  } else if (
+    playerSelection.toLowerCase() === "paper" &&
+    computerSelection.toLowerCase() === "scissors"
+  ) {
+    computerScore++;
+    return `Computer played ${computerSelection} and you played ${playerSelection} so you lose!`;
+  } else if (playerSelection === computerSelection) {
+    return `Computer played ${computerSelection} and you played ${playerSelection} so it's a tie!`;
+  } else {
+    playerScore++;
+    return `Computer played ${computerSelection} and you played ${playerSelection} so you win!`;
+  }
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
