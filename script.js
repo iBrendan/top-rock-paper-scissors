7145a401bb3ac10f3d3d79c918a966f05f7ce2c6
iBrendan <imohbren@gmail.com>
2022-08-07 23:09:58 -0400

    Play 5 rounds, keep score and declare game winner

--- script.js
+++ script.js
@@ -34,7 +34,31 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
+// const playerSelection = "scissors";
+// const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  for (i = 0; i < 5; i++) {
+    playerSelection = prompt("Your turn to play");
+    computerSelection = getComputerChoice();
+    const game = playRound(playerSelection, computerSelection);
+    console.log(game);
+  }
+  if (computerScore > playerScore) {
+    console.log(
+      `Computer score is ${computerScore} and your score is ${playerScore} so you lose`
+    );
+  } else if (playerScore === computerScore) {
+    console.log(
+      `Computer score is ${computerScore} and your score is ${playerScore} so game is a tie`
+    );
+  } else {
+    console.log(
+      `Computer score is ${computerScore} and your score is ${playerScore} so you win the game`
+    );
+  }
+}
+
+game();
